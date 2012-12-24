db9b7c0bc2032d2bff716efffcf1e566ebde9ebc
Rhys Elsmore <rhys@elsmore.id.au>
2012-12-24 16:28:18 +1100

    Added Header Support, and Static List of Supported Methods

--- flask_method_rewrite.py
+++ flask_method_rewrite.py
@@ -6,6 +6,17 @@ from werkzeug import url_decode
 class Middleware(object):
     """ WSGI Method Rewriting Middleware """
 
+    allowed_methods = frozenset([
+        'GET',
+        'HEAD',
+        'POST',
+        'DELETE',
+        'PUT',
+        'PATCH',
+        'OPTIONS'
+    ])
+    bodyless_methods = frozenset(['GET', 'HEAD', 'OPTIONS', 'DELETE'])
+
     def __init__(self, app=None, input_name=''):
         self.app = app
         self.input_name = input_name
@@ -13,12 +24,26 @@ class Middleware(object):
     def __call__(self, environ, response):
         """ Extract the new method from the Query String """
 
-        if 'METHOD_OVERRIDE' in environ.get('QUERY_STRING', ''):
-            args = url_decode(environ['QUERY_STRING'])
-            method = args.get('__METHOD_OVERRIDE__')
-            if method:
+        def set_method(method):
+            if method in self.allowed_methods:
                 method = method.encode('ascii', 'replace')
                 environ['REQUEST_METHOD'] = method
+            if method in self.bodyless_methods:
+                environ['CONTENT_LENGTH'] = '0'
+
+        # Handle our Header Method
+        method = environ.get('HTTP_X_HTTP_METHOD_OVERRIDE', '').upper()
+        if method in self.allowed_methods:
+            set_method(method)
+            return self.app(environ, response)
+
+        # Handle our Query String Method setter
+        if '_method' in environ.get('QUERY_STRING', ''):
+            args = url_decode(environ['QUERY_STRING'])
+            method = args.get('_method', '').upper()
+            if method in self.allowed_methods:
+                set_method(method)
+
         return self.app(environ, response)
 
 
--- test_flask_method_rewrite.py
+++ test_flask_method_rewrite.py
@@ -1,22 +1,53 @@
-from flask import Flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from flask import Flask, request
 from flask_method_rewrite import MethodRewrite
 
-app = Flask(__name__)
-method_rewrite = MethodRewrite(app)
 
-@app.route('/')
-def index():
-    return """
-    <form action="/?__METHOD_OVERRIDE__=PUT" method='POST'>
-        <input type="text" name="testing"/>
-        <input type="submit">
-    </form>
-    """
+class MethodRewriteTestCase(unittest.TestCase):
 
-@app.route('/',methods=['PUT'])
-def put():
-    return "OK"
+    def setUp(self):
+        """ Construct our Flask Test App """
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+        def index():
+            return "get"
+
+        def put():
+            return "put"
+
+        self.app = Flask(__name__)
+        method_rewrite = MethodRewrite()
+        method_rewrite.init_app(self.app)
+        self.app.add_url_rule('/', 'index', index, methods=['GET'])
+        self.app.add_url_rule('/', 'put', put, methods=['PUT'])
+        self.client = self.app.test_client()
+
+    def test_get(self):
+        """ Test get """
+
+        assert "get" in self.client.get('/').data
+
+    def test_query_string(self):
+        """ Test put """
+
+        rv = self.client.put('/')
+        assert "put" in rv.data
+        rv = self.client.post('/?_method=put')
+        assert "put" in rv.data
+
+        with self.app.test_client() as c:
+            c.post('/?_method=put')
+            assert request.method == 'PUT'
+
+    def test_x_header(self):
+        with self.app.test_client() as c:
+            c.post(
+                '/',
+                headers=[('X-HTTP-Method-Override', 'put')]
+            )
+            assert request.method == 'PUT'
+
+if __name__ == '__main__':
+    unittest.main()
